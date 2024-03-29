8329e687fc6acb2b5ffa187bc8b91103a6a209d9
Bret Johnson <bretjohn@microsoft.com>
2024-03-29 17:20:06 -0400

    Add test project files

---
+++ AndroidEnvTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AndroidEnvTest;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AndroidEnvTest/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AndroidEnvTest;
+public static class MauiProgram
+{
+    static void Log(string message) => Trace.WriteLine(message);
+
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        System.Collections.IDictionary variables = Environment.GetEnvironmentVariables();
+        foreach (var key in variables.Keys)
+        {
+            Log($"{key}={variables[key]}");
+        }
+
+        return builder.Build();
+    }
+}
